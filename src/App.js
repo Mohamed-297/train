f83866ceec594bb3df9b9d79e94bfe0dba668e90
mohamed <mohamedelsherif671@gmail.com>
2026-09-21 16:50:36 +0300

    styling and adding to login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Link, Route,Routes } from 'react-router-dom';
-import Login from './login';
+import Login from "./Pages/Login/Login.jsx";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          
-          <Route path='/login' element={<Login/>} /> 
-        
+          <Route path='/login' element={<Login/>} />         
         </Routes>
         <button><Link to="/login">To Login</Link></button>
       </header>
